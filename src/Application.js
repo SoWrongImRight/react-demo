f29467032721017e085e341bc2ba601bb1fbe346
Russ Carroll <info@rcarroll.dev>
2021-06-28 23:55:46 -0400

    Created props, state, and related functions.

--- src/Application.js
+++ src/Application.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
+import HighScore from './HighScore';
 
 class Application extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      overTen: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  componentDidUpdate(props, state) {
+    if (
+      this.state.count > 10 &&
+      this.state.count !== state.count &&
+      !this.state.overTen
+    ) {
+      console.log('Updating over ten');
+      this.setState({ overTen: true });
+    }
+  }
+
+  resetCount = (e) => {
+    console.log('Event is', e);
+    this.setState({
+      count: 0,
+      overTen: false,
+    });
+  };
+
   render() {
-    let name = 'Joe';
+    let { count } = this.state;
+    // let name = 'Joe';
     return (
       <div>
-        <h1>Hello, {name}</h1>
-        <span>This</span>
+        <h1>You clikced the button {count} times.</h1>
+        <HighScore overTen={this.state.overTen} onReset={this.resetCount} />
+        <span>
+          <button onClick={(e) => this.handleClick()}>Click Me</button>
+        </span>
       </div>
     );
   }
